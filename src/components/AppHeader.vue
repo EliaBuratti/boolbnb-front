4682c7a961f4906dedc016dd0ef0affdeeb3bf22
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-12-12 22:07:19 +0100

    add logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,7 +8,7 @@ export default {
     <header class="bg-white py-3 w-100 sticky-top">
         <nav class="navbar navbar-expand-sm navbar-light">
             <div class="container">
-                <router-link class="navbar-brand" to="/">BoolBnb Logo</router-link>
+                <router-link class="navbar-brand" to="/"><img src="../assets/img/Boolbnb.png" width="100"></router-link>
                 <div class="d-flex gap-3">
                     <a class="nav-link active" aria-current="page" href="http://127.0.0.1:8000/login">Login</a>
                     <a class="nav-link active" aria-current="page" href="http://127.0.0.1:8000/register">Register</a>
