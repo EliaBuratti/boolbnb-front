c7adca94faf4585dd12b4b5ec01b3a0f70568fa0
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-12-12 11:14:00 +0100

    addSearchView

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 
 import AppHome from './view/AppHome.vue';
+import AppSearch from './view/AppSearch.vue';
 
 const routes = [
     { path: '/', name: 'Home', component: AppHome },
+    { path: '/search', name: 'Search', component: AppSearch },
     //{ path: '/Projects/:slug', name: 'Single-Project', component: SingleProject },
     //{ path: '/:pathMatch(.*)*', name: 'NotFound', component: App404 },
 ];
